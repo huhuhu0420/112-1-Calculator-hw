335b383d2f104d8e6758adb1dc1d650bb5dddb42
huhuhu0420 <ghostcheng.00@gmail.com>
2023-09-15 20:38:18 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.equalBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(493, 75);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 22);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(458, 149);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(62, 54);
+            this.num1.TabIndex = 1;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(570, 149);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(62, 54);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Location = new System.Drawing.Point(688, 240);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(62, 54);
+            this.plusBtn.TabIndex = 1;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // equalBtn
+            // 
+            this.equalBtn.Location = new System.Drawing.Point(688, 312);
+            this.equalBtn.Name = "equalBtn";
+            this.equalBtn.Size = new System.Drawing.Size(62, 54);
+            this.equalBtn.TabIndex = 1;
+            this.equalBtn.Text = "=";
+            this.equalBtn.UseVisualStyleBackColor = true;
+            this.equalBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.equalBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button equalBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            model = new Model();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            textBox.Text = model.GetResult().ToString();
+        }
+
+        private Model model;
+    }
+
+    public partial class Model
+    {
+        public Model() { }
+        public double GetResult()
+        { 
+        
+            return result;
+        }
+
+        private double result = 0;
+        private double[] nums = { 0, 0 };
+
+    }
+}
